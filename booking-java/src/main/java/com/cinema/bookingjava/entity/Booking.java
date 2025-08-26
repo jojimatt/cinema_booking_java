1222379d1ea64fa9f73acfaba60db43d80d52904
jojimatt <jojimattz@gmail.com>
2025-08-26 17:14:03 +0200

    booking and movie constructor

--- booking-java/src/main/java/com/cinema/bookingjava/entity/Booking.java
+++ booking-java/src/main/java/com/cinema/bookingjava/entity/Booking.java
@@ -1,4 +1,13 @@
 package com.cinema.bookingjava.entity;
 
 public class Booking {
+    private String user;
+    private Movie movie;
+    private Seat seat;
+
+    public Booking(String user, Movie movie, Seat seat) {
+        this.user = user;
+        this.movie = movie;
+        this.seat = seat;
+    }
 }
--- booking-java/src/main/java/com/cinema/bookingjava/entity/Movie.java
+++ booking-java/src/main/java/com/cinema/bookingjava/entity/Movie.java
@@ -1,4 +1,13 @@
 package com.cinema.bookingjava.entity;
 
 public class Movie {
+    String title;
+    String genre;
+    String runtime;
+
+    public Movie(String title, String genre, String runtime) {
+        this.title = title;
+        this.genre = genre;
+        this.runtime = runtime;
+    }
 }
