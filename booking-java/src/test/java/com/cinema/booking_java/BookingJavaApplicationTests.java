8275f0a8ff1a7a8e72a21138c092989ee15781d7
jojimatt <jojimattz@gmail.com>
2025-08-26 15:41:02 +0200

    file structure

---
+++ booking-java/src/test/java/com/cinema/booking_java/BookingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinema.booking_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
