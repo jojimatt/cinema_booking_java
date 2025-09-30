8fc98ea07e8243618fb57f6ff5e05a6e0f961d20
jojimatt <jojimattz@gmail.com>
2025-09-30 13:50:14 +0200

    show and screen dao

---
+++ booking-java/src/main/java/com/cinema/bookingjava/entity/Screen.java
@@ -0,0 +1,8 @@
+package com.cinema.bookingjava.entity;
+
+public class Screen {
+    private int screen_id;
+    String screen_number;
+    int total_seats;
+
+}
---
+++ booking-java/src/main/java/com/cinema/bookingjava/entity/Show.java
@@ -0,0 +1,14 @@
+package com.cinema.bookingjava.entity;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Show {
+    private int show_id;
+    private Movie movie;
+    private Screen screen;
+    private LocalDateTime show_time;
+    private int available_seats;
+    private int booked_seats;
+
+}
