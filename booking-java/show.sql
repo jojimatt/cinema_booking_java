360a6c32c579ef5a21a9b4d06de052d87d6311d0
jojimatt <jojimattz@gmail.com>
2025-10-18 13:28:00 +0200

    updated shows wiht booked and available seats for basic ticket bookin

--- booking-java/show.sql
+++ booking-java/show.sql
@@ -1,38 +1,38 @@
-INSERT INTO shows (screen_id, movie_id, show_time)
+INSERT INTO shows (screen_id, movie_id, show_time, available_seats,booked_seats)
 VALUES (
     1,
     (SELECT movie_id FROM movies WHERE title = 'Inception'),
-    '2025-10-09 18:30:00'
+    '2025-10-09 18:30:00',(SELECT total_seats FROM screens WHERE screen_id = 1) ,0
 ),
 (
     1,
     (SELECT movie_id FROM movies WHERE title = 'Premam'),
-    '2025-10-09 09:30:00'
+    '2025-10-09 09:30:00',(SELECT total_seats FROM screens WHERE screen_id = 1),0
 ),
 (
     2,
     (SELECT movie_id FROM movies WHERE title = '3 Idiots'),
-    '2025-10-09 18:30:00'
+    '2025-10-09 18:30:00',(SELECT total_seats FROM screens WHERE screen_id = 2),0
 ),
 (
     4,
     (SELECT movie_id FROM movies WHERE title = 'F1'),
-    '2025-10-09 15:30:00'
+    '2025-10-09 15:30:00',(SELECT total_seats FROM screens WHERE screen_id = 4),0
 ),
 (
     4,
     (SELECT movie_id FROM movies WHERE title = 'Top Gun: Maverick'),
-    '2025-10-09 18:30:00'
+    '2025-10-09 18:30:00',(SELECT total_seats FROM screens WHERE screen_id = 1),0
 ),
 (
     3,
     (SELECT movie_id FROM movies WHERE title = 'Inception'),
-    '2025-10-09 21:30:00'
+    '2025-10-09 21:30:00',(SELECT total_seats FROM screens WHERE screen_id = 3),0
 ),
 (
     3,
     (SELECT movie_id FROM movies WHERE title = 'Interstellar'),
-    '2025-10-09 11:30:00'
+    '2025-10-09 11:30:00',(SELECT total_seats FROM screens WHERE screen_id = 3),0
 )
 
 
---
+++ booking-java/src/main/java/com/cinema/bookingjava/services/DatabaseOperations.java
@@ -0,0 +1,4 @@
+package com.cinema.bookingjava.services;
+
+public class DatabaseOperations {
+}
